973a4dc9756e58887d35ab5a7450391d54b38a45
iamrajvardhanmall <rajvardhanmall@gmail.com>
2025-09-21 20:03:16 +0530

    Image inside a Template

--- myProject/myApp/urls.py
+++ myProject/myApp/urls.py
@@ -23,4 +23,10 @@ urlpatterns = [
     path('myimagepage/', views.myimagepage, name="myimagepage"),
 
     path('myimagepage2/', views.myimagepage2, name="myimagepage2"),
+
+    path('myimagepage3/', views.myimagepage3, name="myimagepage3"),
+
+    path('myimagepage4/', views.myimagepage4, name="myimagepage4"),
+
+    path('myimagepage5/<str:imagename>/', views.myimagepage5, name="myimagepage5"),
 ]
--- myProject/myApp/views.py
+++ myProject/myApp/views.py
@@ -50,3 +50,22 @@ def myimagepage(request):
 
 def myimagepage2(request):
     return render(request, 'imagepage2.html')
+
+def myimagepage3(request):
+    return render(request, 'imagepage3.html')
+
+def myimagepage4(request):
+    return render(request, 'imagepage4.html')
+
+def myimagepage5(request, imagename):
+    myimagename = imagename
+    myimagename = myimagename.lower()
+    print(myimagename)
+    if myimagename == "django":
+        var = True
+    elif myimagename == "python":
+        var = False
+    mydictionary = {
+        "var": var
+    }
+    return render(request, 'imagepage5.html', context=mydictionary)
