d5ad89dfbdf2e23f404840802906ccb61ec7cc1b
Rajvardhan Mall <144551761+iamrajvardhanmall@users.noreply.github.com>
2026-02-16 22:42:54 +0530

    Delete myProject directory

--- myProject/myApp/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-class feedbackForm(forms.Form):
-    title = forms.CharField(label = 'Title', max_length = 50, widget= forms.TextInput(attrs={'class':'form-control','placeholder':'Enter Title Here'}))
-    # class and placeholder are added as attributes to the widget
-    
-    subject = forms.CharField(label = 'Subject Description', max_length = 200, widget = forms.Textarea)
-
-
