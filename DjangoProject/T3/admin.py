79d4b965a41902d77fb2fe6f551bbbc872462809
iamrajvardhanmall <rajvardhanmall@gmail.com>
2026-05-20 21:27:28 +0530

    Django QuerySet - Order By

--- DjangoProject/T3/admin.py
+++ DjangoProject/T3/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import user
+from .models import user, EmployeeDetails
 # Register your models here.
 
 admin.site.register(user)
+admin.site.register(EmployeeDetails)
--- DjangoProject/T3/models.py
+++ DjangoProject/T3/models.py
@@ -8,3 +8,14 @@ class user(models.Model):
     
     def __str__(self):
         return self.user_name
+    
+class EmployeeDetails(models.Model):
+    EmployeeId = models.AutoField(primary_key=True)
+    EmployeeName = models.CharField(max_length=20)
+    EmployeeDepartment = models.CharField(max_length=20, blank=True, null=True)
+    Country = models.CharField(max_length=20, blank=True, null=True)
+    Salary = models.IntegerField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.EmployeeName
+    
