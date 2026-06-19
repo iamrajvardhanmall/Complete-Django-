7980f6ca59e508d9866e635f62756c6111a18f5c
iamrajvardhanmall <rajvardhanmall@gmail.com>
2026-06-19 21:06:59 +0530

    Django Templates

--- DjangoProject/T7_Django_Templates/urls.py
+++ DjangoProject/T7_Django_Templates/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from T7_Django_Templates import views  # App is name "T7_Django_Templates"
 
 urlpatterns = [
+    path('simple/', views.simple_view),
+    path('condition/', views.check_age),
+    path('loop/', views.loop),
     
 ]
--- DjangoProject/T7_Django_Templates/views.py
+++ DjangoProject/T7_Django_Templates/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+def simple_view(request):
+    context = {"data": "Raj is the best"}
+    return render(request, "raj.html", context)
+
+def check_age(request):
+    age = None
+    if request.method == 'POST':
+        # request.POST.get returns a string, default to "0"
+        age = int(request.POST.get('age', 0))
+    return render(request, 'check_age.html', {'age': age})
+
+def loop(request):
+    data = "Raj is the Best"
+    number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    context = {
+        "data": data,
+        "list": number_list
+    }
+    return render(request, "loop.html", context)
